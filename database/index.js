3dc4fd07e249589ff58d4c10d53cccc2a1ac72c8
liamohkay <liam.olsonkenny@gmail.com>
2021-02-24 13:33:20 -0800

    Wrote unit test that tests connection to database

--- database/index.js
+++ database/index.js
@@ -1,31 +1,26 @@
 const mongoose = require('mongoose');
 const dbName = 'FEC'
 
-const connectDB = function() {
+// Connect to user specified DB
+mongoose.connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true} )
+  .then(() => console.log(`Connected to database ${dbName}`))
+  .catch(err => console.log(`Error when connecting to database ${dbName}. Error: ${err}`))
 
-  // Connect to user specified DB
-  mongoose.connect(`mongodb://localhost/${dbName}`, { useNewUrlParser: true, useUnifiedTopology: true} )
-    .then(() => console.log(`Connected to database ${dbName}`))
-    .catch(err => console.log(`Error when connecting to database ${dbName}. Error: ${err}`))
+// Schema for Product instances in Products collections
+let productSchema = new mongoose.Schema({
+  id: Number,
+  name: String
+});
 
-  // Schema for Product instances in Products collections
-  let productSchema = new mongoose.Schema({
-    id: Number,
-    name: String
-  });
+// Add schema to db + temporarily intialize with dummy entry
+let Product = mongoose.model('Product', productSchema);
 
-  // Add schema to db + temporarily intialize with dummy entry
-  let Product = mongoose.model('Product', productSchema);
-
-  Product.create({
-    id: 1,
-    name: 'Sophia'
-  });
-
-}();
+Product.create({
+  id: 1,
+  name: 'Sophia'
+});
 
 // Export dbName & connectDB
 module.exports = {
-  dbName,
-  connectDB
+  dbName
 };
--- spec/database.js
+++ spec/database.js
@@ -1,5 +1,17 @@
-const chai = require('chai');
-const mocha = require('mocha');
+// Library dependencies
+const mongoose = require('mongoose');
+// File dependencies
+const db = require('../database/index.js');
+
+/* ----------------
+Database Unit Tests
+---------------- */
+describe('Mongo Database', () => {
+  it(`Sucessfully connects to MongoDBS ${db.dbName}`, done => {
+    mongoose.connect(`mongodb://localhost/${db.dbName}`, done);
+  });
+});
+
 // describe('Mongo Database', function() {
 //   before(function (done) {
 //     mongoose.connect('mongodb://localhost/FEC', done);
