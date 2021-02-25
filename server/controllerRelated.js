fd6ac4a1db8891c408cb51c3ceaf26a704026a85
Jordan Beaird <jordan.beaird@gmail.com>
2021-02-25 09:37:34 -0800

    Refactored controllerRelated.js to have nested headers object

---
+++ server/controllerRelated.js
@@ -0,0 +1,38 @@
+const axios = require('axios');
+const config = require('./config.js')
+// const productId = require('./........')
+
+const controllerRelated = {
+  getRelatedProducts: (req, res) => {
+    let options = {
+      url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/${req.params.product_id}/related`,
+      headers: {
+        headers: {
+          'User-Agent': 'request',
+          'Authorization': `${config.TOKEN}`
+        }
+      }
+    }
+
+
+    axios.get(options.url, options.headers)
+      .then((results) => res.send(results.data))
+      .catch((err) => res.send(err))
+  }
+}
+
+
+
+// https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/products/:product_id/related
+
+module.exports = controllerRelated;
+
+
+
+
+
+
+
+
+
+// GET /products/:product_id/related
--- server/router.js
+++ server/router.js
@@ -1,14 +1,23 @@
 const router = require('express').Router();
 const controller = require('./controller');
+const controllerRelated = require('./controllerRelated.js')
 
 router
   .route('/')
-  .get(controller.get)
+  // .get(controller.get)
   .post(controller.post)
+  .get(controllerRelated.getRelatedProducts)
+
 
 router
   .route('/')
   .patch(controller.update)
   .delete(controller.delete)
 
-  module.exports = router;
+  router
+  .route('/:product_id')
+  .get(controllerRelated.getRelatedProducts)
+
+
+
+module.exports = router;
