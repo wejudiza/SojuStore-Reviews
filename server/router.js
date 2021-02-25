3449d61d58ec6d8d7039e08559c48e375fdc388c
sophiacheong <sophiaacheong@gmail.com>
2021-02-25 09:36:05 -0800

    Added comment to your routes + controllers

--- server/router.js
+++ server/router.js
@@ -3,6 +3,7 @@ const controller = require('./controller.js');
 
 router
   .route('/')
+    // to retrieve the list of products
     .get(controller.get)
     .post(controller.post)
 
@@ -13,10 +14,12 @@ router
 
 router
   .route('/:id')
+    // to retrieve all product level information for a SPECIFIC product ID
     .get(controller.getProducts)
 
 router
   .route('/styles/:id')
+  // to retrieve all styles available for the given product
     .get(controller.getProductStyles)
 
  module.exports = router;
