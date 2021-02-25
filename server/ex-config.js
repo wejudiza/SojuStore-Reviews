05c35cf24871a3eda1dfec1fbe68f6fd6151ce9a
liamohkay <liam.olsonkenny@gmail.com>
2021-02-25 12:28:15 -0800

    Updated ex-config.js

--- server/ex-config.js
+++ server/ex-config.js
@@ -1,9 +1,12 @@
 const token = 'YOUR PAT'
 
 module.exports = {
-  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax/',
+  url: 'https://app-hrsei-api.herokuapp.com/api/fec2/hr-lax',
   headers: {
+    headers: {
     'User-Agent': 'request',
     'Authorization': `${token}`
-  }
+    }
+  },
+  token: token
 }
