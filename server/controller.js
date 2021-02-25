9825f390c9c2738fc640d3f0cba34c2ae3a9d37b
Daniel <danielliu1824@gmail.com>
2021-02-25 09:45:35 -0800

    Added QnA get

--- server/controller.js
+++ server/controller.js
@@ -1,8 +1,15 @@
 //require models
+const getQnA = require('./QnAGet.js');
 
 const controller = {
   get: (req, res) => {
-    res.send('get')
+    getQnA.getQnA((err, data) => {
+      if (err) {
+        res.status(400).send(err);
+      } else {
+        res.status(200).send(data);
+      }
+    })
   },
   post: (req, res) => {
     res.send('post')
