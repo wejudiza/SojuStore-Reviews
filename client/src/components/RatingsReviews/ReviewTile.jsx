7916df2dfe3ce85a698f62a85742767d8f816507
liamohkay <liam.olsonkenny@gmail.com>
2021-02-27 11:40:51 -0800

    Fixed used tile to send 1 api call on mount

--- client/src/components/RatingsReviews/ReviewTile.jsx
+++ client/src/components/RatingsReviews/ReviewTile.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 // import ReviewSummary from './ReviewSummary';
 
-export default function ReviewTile(props) {
+export default function ReviewTile() {
   const [review, setReview] = useState();
 
   // product_id: props.product_id
-  // Get reviews from Atellier API for specific product + set to state
+  // Get reviews from Atellier API for specific product + set to intial state
   let product_id = 16821;
-  axios.get(`api/reviews/${product_id}`)
-    .then((resp) => setReview(resp.data))
-    .catch((err) => alert(err));
+  useEffect(
+    () => {
+      axios.get(`api/reviews/${product_id}`)
+        .then((resp) => setReview(resp.data))
+        .catch((err) => alert(err));
+    }, []
+  );
 
   return (
     <div></div>
