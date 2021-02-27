4f7bfd623a85bad07c6ec63b1e906f78d35079da
Daniel <danielliu1824@gmail.com>
2021-02-27 12:24:26 -0800

    Created qna component

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import RelatedItems from './RelatedItems.jsx';
 import ProductInfo from './Overview/ProductInfo.jsx';
+import QnA from './QnA.jsx';
 
 export default class App extends Component {
   constructor(props) {
@@ -15,6 +16,7 @@ export default class App extends Component {
         Testing webpack link
         <ProductInfo />
         <RelatedItems />
+        <QnA />
       </div>
     );
   }
