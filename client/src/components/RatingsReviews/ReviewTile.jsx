d20403d77e31130b2f502a3bedf1bfe3040691af
liamohkay <liam.olsonkenny@gmail.com>
2021-02-27 15:00:49 -0800

    Added img tags to review-body. Will need to transform pictures so they are perfectly square probably

--- client/src/components/RatingsReviews/ReviewTile.jsx
+++ client/src/components/RatingsReviews/ReviewTile.jsx
@@ -1,14 +1,25 @@
 import React, { useState, useEffect } from 'react';
+import Thumbnails from './Thumbnails.jsx';
 
 export default function ReviewTile(props) {
   const [review, setReview] = useState(props.review);
 
   return (
     <div className="review-tile">
-      {/* Review Summary + Body */}
+      { /* Username + date submitted */ }
+      <div className="review-userinfo">
+
+      </div>
+      { /* Review Summary + Body */ }
       <div className="review-section">
         <h3 className="review-summary">{review.summary}</h3>
-        <p className="review-body">{review.body}</p>
+        <p className="review-body">
+          {review.body}
+          <br></br>
+          <div className="review-thumbnails">
+            { review.photos.map((photo) => <img src={photo.url} alt={photo.id} key={photo.id} />)}
+          </div>
+        </p>
       </div>
       { /* Recommended conditional render */ }
       <div className="user-recommended">
@@ -16,7 +27,7 @@ export default function ReviewTile(props) {
       </div>
       { /* Response conditional render to review */ }
       <div className="review-response">
-        { review.response ? <div>{review.response}</div> : null }
+        { review.response ? <div>{ review.response }</div> : null }
       </div>
     </div>
   );
