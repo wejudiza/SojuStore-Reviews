06fe0cb88a30a48f78001b461c63886f3b710dcb
sophiacheong <sophiaacheong@gmail.com>
2021-02-27 12:42:16 -0800

    accepted incoming chnages

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,12 +1,6 @@
 import React, { Component } from 'react';
-// import axios from 'axios';
-<<<<<<< HEAD
-import RelatedItems from './RelatedItems.jsx';
-import Product from './Overview/Product.jsx';
-=======
 import RelatedProducts from './RelatedProducts.jsx';
-import ProductInfo from './Overview/ProductInfo.jsx';
->>>>>>> 36f6242d5cde0b678dab6398f588e9870d7e3418
+import Product from './Overview/Product.jsx';
 
 // Import RatingsReviews Components
 import SortSelect from './RatingsReviews/SortSelect.jsx';
@@ -23,13 +17,8 @@ export default class App extends Component {
     return (
       <div>
         Testing webpack link
-<<<<<<< HEAD
         <Product />
-        <RelatedItems />
-=======
-        <ProductInfo />
         <RelatedProducts />
->>>>>>> 36f6242d5cde0b678dab6398f588e9870d7e3418
 
       {/* --- Ratings & Reviews --- */}
       <div id="ratings-reviews">
