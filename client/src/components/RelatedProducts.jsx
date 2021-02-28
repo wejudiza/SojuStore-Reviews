cf642a61ab9b81a4d52709c1e71cf54f07ef9e91
Jordan Beaird <jordan.beaird@gmail.com>
2021-02-28 13:11:04 -0800

    Added CSS to position button on top of image

--- client/src/components/RelatedProducts.jsx
+++ client/src/components/RelatedProducts.jsx
@@ -18,12 +18,12 @@ class RelatedProducts extends React.Component {
     };
     this.toggleModal = this.toggleModal.bind(this);
     this.getInfo = this.getInfo.bind(this);
-    this.getStyles = this.getStyles.bind(this);
+    // this.getStyles = this.getStyles.bind(this);
   }
 
   componentDidMount() {
     this.getInfo(this.props.productId);
-    this.getStyles(this.props.productId);
+    // this.getStyles(this.props.productId);
   }
 
   getInfo(id) {
@@ -36,18 +36,26 @@ class RelatedProducts extends React.Component {
           features: response.data.features
         })
       })
-  }
-
-  getStyles(id) {
     axios.get(`api/styles/${id}`)
-      .then((response) => {
-        console.log(response.data.results)
-        this.setState({
+    .then((response) => {
+      console.log(response.data.results)
+      this.setState({
         original_price: response.data.results[0].original_price,
         sale_price: response.data.results[0].sale_price,
         thumbnail_url: response.data.results[0].photos[0].thumbnail_url
-      })})
+    })})
   }
+
+  // getStyles(id) {
+  //   axios.get(`api/styles/${id}`)
+  //     .then((response) => {
+  //       console.log(response.data.results)
+  //       this.setState({
+  //       original_price: response.data.results[0].original_price,
+  //       sale_price: response.data.results[0].sale_price,
+  //       thumbnail_url: response.data.results[0].photos[0].thumbnail_url
+  //     })})
+  // }
 
   toggleModal() {
     this.setState({
@@ -58,25 +66,25 @@ class RelatedProducts extends React.Component {
   render() {
     return (
       <div className="related-products-container">
-        <button onClick={this.toggleModal}>open modal</button>
-        <div>
-          <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.toggleModal}>
-            <h2>Modal Title</h2>
-            <p>Model Body</p>
-            <button onClick={this.toggleModal}>close</button>
-          </Modal>
-        </div>
-        <div>
+        <div id="test">
           <img src={this.state.thumbnail_url}></img>
-        </div>
-        <div>
-          {this.state.category}
-        </div>
-        <div>
-          {this.state.name}
-        </div>
-        <div>
-          {this.state.original_price}
+          <button onClick={this.toggleModal} className='btn'>open modal</button>
+          <div>
+            <Modal isOpen={this.state.modalIsOpen} onRequestClose={this.toggleModal}>
+              <h2>Modal Title</h2>
+              <p>Model Body</p>
+              <button onClick={this.toggleModal}>close</button>
+            </Modal>
+          </div>
+          <div>
+            {this.state.category}
+          </div>
+          <div>
+            {this.state.name}
+          </div>
+          <div>
+            {'$ ' + this.state.original_price}
+          </div>
         </div>
       </div>
     );
