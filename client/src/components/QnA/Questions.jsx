96cbdb51aa62dfd377608628a7789c9e35521133
Daniel <danielliu1824@gmail.com>
2021-02-28 01:56:53 -0800

    Added show more answer functionality

--- client/src/components/QnA/Questions.jsx
+++ client/src/components/QnA/Questions.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Answer from './Answers.jsx';
 
 export default function Question({question, index}) {
@@ -6,17 +6,26 @@ export default function Question({question, index}) {
     return question.answers[key]
   });
 
+  const [answersToShow, setAnswerstoShow] = useState(2);
+
+  const showMoreAnswers = () => {
+    setAnswerstoShow(array.length)
+  }
+
   return(
     <div className="question">
       <h4>
       Q: {question.question_body}
       </h4>
        <div>
-        {array.map((answer, index) =>
+        {array.slice(0,answersToShow).map((answer, index) =>
         <div key={index}>
             <Answer answer={answer}/>
         </div>
         )}
+        <div>
+          <button onClick={showMoreAnswers}>Load More Answers</button>
+        </div>
         {console.log('answer:', array)}
       </div>
     </div>
