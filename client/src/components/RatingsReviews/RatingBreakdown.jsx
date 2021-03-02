7c14fc83c67a0c533d899a1dcdb22eb5f7c22ac2
liamohkay <liam.olsonkenny@gmail.com>
2021-03-02 10:29:53 -0800

    Calculated % of people who recommend

--- client/src/components/RatingsReviews/RatingBreakdown.jsx
+++ client/src/components/RatingsReviews/RatingBreakdown.jsx
@@ -3,17 +3,24 @@ import React, { useState, useEffect } from 'react';
 export default function RatingBreakdown({ allReviews, reviewsCount }) {
   const [percentage, setPercentage] = useState(100);
 
+  // On render + change in # of reviews calculate % of people who recommend
   useEffect(() => {
     let count = 0;
+
     allReviews.map((review) => {
-      if (reivew.recommend)
+      if (review.recommend) {
+        count += 1;
+      }
     });
-  }, [allReviews]);
+
+    setPercentage(count / reviewsCount);
+  }, [allReviews, reviewsCount]);
+
 
   return (
     <div id="RatingBreakdown">
       { reviewsCount === 0 ? null : (
-        <div></div>
+        <div>{percentage}</div>
       ) }
     </div>
   );
