4761b766d38d61abefed41a6177332e079c8bb6c
liamohkay <liam.olsonkenny@gmail.com>
2021-03-03 19:52:34 -0800

    Set up AddReview modal boiler plate

---
+++ client/src/components/RatingsReviews/AddReview.jsx
@@ -0,0 +1,31 @@
+import React, { useState, useContext } from 'react';
+import Modal from 'react-modal';
+
+// Contexts
+import { UserContext } from '../UserContext.jsx';
+
+export default function AddReview() {
+  const [isOpen, setIsOpen] = useState(false);
+  let product = useContext(UserContext);
+
+  return (
+    <div id="add-review">
+      <button id="add-review-btn" type="button" onClick={() => setIsOpen(true)}>Add A Review +</button>
+
+      <Modal id="add-review-modal" isOpen={isOpen}>
+
+        { /* Dynamic review header / title */ }
+        <div id="add-review-header">
+          Write Your Review For
+          <br />
+          { product.name }
+        </div>
+
+
+
+        <button id="close-review-btn" type="button" onClick={() => setIsOpen(false)}>Close</button>
+      </Modal>
+
+    </div>
+  );
+}
--- client/src/components/RatingsReviews/RatingsReviews.jsx
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -1,7 +1,11 @@
+/* ---------------------------------
+Library Dependencies + Subcomponents
+--------------------------------- */
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-// Import components
+// Subcomponents
+import AddReview from './AddReview.jsx';
 import SortSelect from './SortSelect.jsx';
 import ReviewTile from './ReviewTile.jsx';
 import RatingBreakdown from './RatingBreakdown.jsx';
@@ -10,13 +14,16 @@ import ProductBreakdown from './ProductBreakdown.jsx';
 // Dummy product_id
 const product_id = 16500;
 
+/* ------------------------
+Ratings & Reviews Component
+------------------------ */
 export default function RatingsReviews() {
   const [allReviews, setAllReviews] = useState([]);
   const [loaded, setLoaded] = useState(false);
   const [numReviews, setNumReviews] = useState(0);
   const [sortBy, setSort] = useState('relevant');
 
-  // Get all reviews from Atellier API for specific product + set to intial state on mount
+  // Get all reviews from Atellier API for specific product + assign to state once loaded
   useEffect(() => {
     axios.get(`/api/reviews/${product_id}`)
       .then((resp) => setAllReviews(resp.data.results))
@@ -25,17 +32,17 @@ export default function RatingsReviews() {
       .catch((err) => console.log(err));
   }, [loaded]);
 
-  // On change event to set sortBy state
+  // On change event handler to set sortBy state (<SortSelect />)
   const handleSelect = (e) => setSort(e.target.value);
 
   return (
     <div className="ratings-reviews">
+      {/* <h3>Ratings & Reviews</h3>
       { /* Rating Breakdown */ }
-      <h3>Ratings & Reviews</h3>
       <RatingBreakdown allReviews={allReviews} numReviews={numReviews} />
 
       { /* Proudct Breakdown */ }
-      <ProductBreakdown />
+      {/* <ProductBreakdown /> */}
 
       { /* Sorting dropdown */ }
       {/* <div id="sortby">
@@ -48,6 +55,9 @@ export default function RatingsReviews() {
         { allReviews.slice(0, 2).map((review) => <ReviewTile review={review} key={review.id} />) }
         { allReviews.slice(0, 2).map((review) => console.log(review)) }
       </div> */}
+
+      { /* Add A Review + Modal */ }
+      <AddReview />
     </div>
   );
 }
