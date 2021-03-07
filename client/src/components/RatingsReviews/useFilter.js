3c0d2c2f4c477c7595b7488c1864586f71720c9d
liamohkay <liam.olsonkenny@gmail.com>
2021-03-07 12:39:59 -0800

    Removed superfluous code

--- client/src/components/RatingsReviews/useFilter.js
+++ client/src/components/RatingsReviews/useFilter.js
@@ -6,18 +6,7 @@ export default function useFilter(initialValues) {
     values,
     (e) => {
       values[e] = !values[e];
-      // const check = Object.values(values).every((val) => val === false);
-      // if (check) {
-      //   setValues({
-      //     5: true,
-      //     4: true,
-      //     3: true,
-      //     2: true,
-      //     1: true,
-      //   });
-      // } else {
       setValues({ ...values });
-      // }
     }
   ];
 }
