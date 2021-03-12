46f0145f4dc131a07d485b09fa5bb9d17dd091f6
Jordan Beaird <jordan.beaird@gmail.com>
2021-03-12 09:38:03 -0800

    Compressed bundle, working on api call tests

---
+++ spec/mocks/handlers.js
@@ -0,0 +1,13 @@
+import { rest } from 'msw';
+
+
+export const handlers = [
+  rest.get('/api', (req, res, ctx) => {
+    return res(
+      ctx.status(200),
+      // ctx.json({
+      //   username: 'admin',
+      // }),
+    )
+  }),
+]
